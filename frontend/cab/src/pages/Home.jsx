325812ac0a7db95aa58a01f93325abaa0464f85c
denma98 <deepak21319@iitd.ac.in>
2023-04-20 01:52:02 +0530

    button added

--- frontend/cab/src/pages/Home.jsx
+++ frontend/cab/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Back = styled.div `
   
   background-image: url(https://images.unsplash.com/photo-1653287886626-ebd9af889e27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1931&q=80);
 
-  display: flex;
+  /* display: flex; */
+  margin-top: -60px;
   height: 100vh;
   width: 1536px;
 `
@@ -20,7 +22,7 @@ const Heading = styled.div`
    margin-left: 25%;
   margin-right:20% ;
   margin-top: 55px;
-  height: 30hv;
+  height: 150px;
   width: 800px;
 `
 
@@ -34,40 +36,60 @@ const SubHeading = styled.p`
   margin-top: 0px;
   width: 900px;
 `
-const ButtonContainer = styled.div`
-  
-  /* display: flex;  */
-  text-align: center;
-  margin-left: 25%;
-  margin-right:20% ;
-  margin-top: 55px;
-  height: 30hv;
-  width: 800px;
-`
 
 const LocationButton = styled.button`
 
-width: 100px;
+width: 600px;
 padding: 10px;
+height: 70px;
 cursor: pointer;
 border: none;
-border-radius: 5px;
-background-color: #da4ea2;
+border-radius: 23px;
+font-size: 20px;
+font-weight: 600;
+background-color: white;
+margin-left: 10%;
 `
+const ButtonContainer = styled.div`
+  
+  display: flex; 
+  color: white;
+  text-align: center;
+  margin-left: 12%;
+  margin-right:auto ;
+  margin-top: 150px;
+  height: 100px;
+  width: 1200px;
+  
+`
+
 
 const Home = () => {
   return (
-    <Back>
+    
+      <>
+      <Back>
+
       <Heading>
-        Wherever you need to go, we've got you covered.
-        <SubHeading>
+      Wherever you need to go, we've got you covered.
+      <SubHeading>
         Book a cab in just a few clicks!
       </SubHeading>
-      </Heading>
+
+    </Heading><ButtonContainer>
+
+        <LocationButton>Current Location</LocationButton>
+        <LocationButton>Destination</LocationButton>
+        <FontAwesomeIcon icon="fa-thin fa-arrow-right-long" />
+        <LocationButton style={{backgroundColor : '#D679B1'}}>Request Ride</LocationButton>
+
+      </ButtonContainer>
+      </Back>
+      
+      </>
       
 
       
-    </Back>
   )
 };
 
